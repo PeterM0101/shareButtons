c7d97ea929d1d1f64c74868991805d4d3ac378ce
Peter Maliar <peter.maliar@tabsandspaces.agency>
2024-11-02 20:44:21 +0200

    Fixed 7

--- components/shared.tsx
+++ components/shared.tsx
@@ -19,17 +19,10 @@ const Shared: FC<SharedProps> = ({url}) => {
         <div
             className={`flex justify-center items-center gap-2`}
         >
-            <FacebookShareButton
-                url={url}
-                quote={'Fuck next-s.'}
-                hashtag={'#nextshare'}
-            >
+            <FacebookShareButton url={url}>
                 <FacebookIcon size={32} round/>
             </FacebookShareButton>
-            <TwitterShareButton
-                url={url}
-                title={'Fuck.'}
-            >
+            <TwitterShareButton url={url}>
                 <TwitterIcon size={32} round/>
             </TwitterShareButton>
             <LinkedinShareButton url={url}>
