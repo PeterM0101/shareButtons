25fc7a925cdf0753139a46b29d5cb036a8c021f1
Peter Maliar <peter.maliar@tabsandspaces.agency>
2025-03-10 19:58:56 +0200

    Fix 18

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import {useEffect} from "react";
 import { init } from "@socialgouv/matomo-next";
 
 // const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-const MATOMO_URL = "cdn.matomo.cloud/sharebuttonshennavercelapp.matomo.cloud/container_LaJCRj3K";
+const MATOMO_URL = "cdn.matomo.cloud/sharebuttonshennavercelapp.matomo.cloud/container_LaJCRj3K.js";
 // const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
 const MATOMO_SITE_ID = ""
 
