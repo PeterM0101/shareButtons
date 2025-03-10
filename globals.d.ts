ae2dad0a637a8185f91eddb1f3967f699ff9139b
Peter Maliar <peter.maliar@tabsandspaces.agency>
2025-03-10 20:28:48 +0200

    Fix 20

--- globals.d.ts
+++ globals.d.ts
@@ -3,5 +3,6 @@ export {}; // Required to make this file a module, preventing scope pollution
 declare global {
     interface Window {
         _mtm: any;
+        _paq: any;
     }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,12 @@ import {useEffect} from "react";
 // import { init } from "@socialgouv/matomo-next";
 
 // const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
-// const MATOMO_URL = "cdn.matomo.cloud/sharebuttonshennavercelapp.matomo.cloud/container_LaJCRj3K.js";
+// const MATOMO_URL = "https://sharebuttonshennavercelapp.matomo.cloud/";
 // const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
-// const MATOMO_SITE_ID = ""
+// const MATOMO_SITE_ID = '1'
 
 export default function App({ Component, pageProps }: AppProps) {
-
+    //
     // useEffect(() => {
     //   init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
     // }, []);
@@ -19,6 +19,18 @@ export default function App({ Component, pageProps }: AppProps) {
     _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src='https://cdn.matomo.cloud/sharebuttonshennavercelapp.matomo.cloud/container_LaJCRj3K.js'; s.parentNode?.insertBefore(g,s);
+
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="https://sharebuttonshennavercelapp.matomo.cloud/";
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.async=true; g.src='https://cdn.matomo.cloud/sharebuttonshennavercelapp.matomo.cloud/matomo.js'; s.parentNode?.insertBefore(g,s);
+    })();
   }, [])
   return <Component {...pageProps} />;
 }
